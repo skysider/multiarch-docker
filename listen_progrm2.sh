cd7f8bebff2a6bdaca3405cb04b4388077863443
shenyuan <junchaoluan@gmail.com>
2018-08-30 23:16:41 +0800

    add script

---
+++ listen_progrm2.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+if [[ -z ${1} ]]; then
+    echo -e "Missing binary path"
+    exit 0
+fi
+if [[ -z ${2} ]]; then
+    echo -e "Missing binary arch"
+    exit 0
+fi
+binary_path=${1}
+arch={2}
+
+socat tcp-listen:2333,reuseaddr,fork exec:"qemu-${arch} -g 1234 ${binary_path}"
